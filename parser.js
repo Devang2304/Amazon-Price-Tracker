95d6a46aa35a58d3a6f5737a2bb0c22e355c010c
Devang2304 <devangvartak2304@gmail.com>
2022-09-06 00:04:30 +0530

    Issue resolved

--- parser.js
+++ parser.js
@@ -1,17 +1,18 @@
-const puppeteer=require('puppeteer')
-// "a-price-whole"
+const puppeteer = require('puppeteer')
 
-// "https://www.amazon.in/dp/B09RQKK6CZ"
+async function scrape() {
+   const browser = await puppeteer.launch({})
+   const page = await browser.newPage()
 
-async function checkPrice() {
-    const priceString=await puppeteer.goto("https://www.amazon.in/dp/B09RQKK6CZ/ref=twister_B0B74LF28D?_encoding=UTF8&th=1")
-                                    .wait("#span .a-offscreen")
-                                    .evaluate(()=>document.getElementById("span .a-offscreen").innerText)
-                                    .end()
-    const priceNumber=parseFloat(priceString.replace('₹',''))
-    if (priceNumber<70000) {
-        console.log("It is cheap");
-    } else {
-        console.log("It is expensive");
-    }
+   await page.goto('https://www.amazon.in/dp/B09RQKK6CZ')
+   var element = await page.waitForSelector("#corePriceDisplay_desktop_feature_div > div.a-section.a-spacing-none.aok-align-center > span.a-price.aok-align-center.reinventPricePriceToPayMargin.priceToPay > span:nth-child(2) > span.a-price-whole")
+   var text = await page.evaluate(element => element.textContent, element)
+   const priceNumber=parseFloat(text.replace(',',''))
+   if (priceNumber<70000) {
+    console.log("It is cheap");
+} else {
+    console.log("It is expensive");
 }
+   browser.close()
+}
+scrape()
